733df6d63519a1b52115d4bb449b4c1923d58ea0
chenwanxing <wanxing182@163.com>
2022-02-15 10:49:30 +0800

    自动化测试

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const a = 1;
-const b = 2;
+// const a = 1;
+// const b = 2;
 
-console.log(a + b);
+// console.log(a + b);
 
 console.log(12);
+
+var express = require('express');
+var app = express()
+
+var router = express.Router()
+
+app.get('/', (req, res) => {
+    res.send('hello world!!!')
+})
+
+app.use('/', router)
+app.listen(1000)
